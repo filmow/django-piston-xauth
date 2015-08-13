51c911834f3453f6d3092fca6654ac267975846a
Thiago Avelino <thiagoavelinoster@gmail.com>
2015-08-13 12:21:48 -0300

    create ConsumerAdmin added raw_id_fields

--- piston/admin.py
+++ piston/admin.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from piston.models import Nonce, Token, Consumer
 
+
+class ConsumerAdmin(admin.ModelAdmin):
+    raw_id_fields = ['user']
+
+
 admin.site.register(Nonce)
 admin.site.register(Token)
-admin.site.register(Consumer)
+admin.site.register(Consumer, ConsumerAdmin)
